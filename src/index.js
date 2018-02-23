4443b2a9bb82259c3ec1fcc584ac8871a0c9f9fc
dmitri.gorchanin <dmitri.gorchanin@MrBrown.local>
2018-02-23 21:40:47 +0300

    done 100%

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 class Sorter {
   constructor() {
     this.array = [];
+    this.comparator = this.defaultComparator;
+
+
   }
   
   add(element) {
@@ -21,32 +24,33 @@ class Sorter {
   }
 
   sort(indices) {
-    function sortfunction(a, b) {
-      return a - b;
+    indices.sort(this.defaultComparator);
+
+    if (indices.length <= 1){
+      return;
     }
-    indices.sort(sortfunction)
-    
-    if (indices.length<=1){
-      return
-    }
+
     var temp_arr = [];
-    for (var i = 0; i<indices.length;i++)
-    {
+
+    for (var i = 0; i < indices.length; i++) {
       temp_arr.push(this.array[indices[i]])
     }
-    function sortfunction(a, b) {
-      return a - b;
-    }
-    temp_arr.sort(sortfunction)
-    for (var i = 0; i<indices.length;i++)
-    {
+
+    temp_arr.sort(this.comparator);
+
+    for (var i = 0; i < indices.length; i++) {
       this.array[indices[i]] = temp_arr[i]
     }
     //console.log(temp_arr)
   }
 
+  defaultComparator(a, b) {
+    return a - b;
+  }
+
   setComparator(compareFunction) {
-    // your implementation
+    this.comparator = compareFunction;
+
   }
 }
 
